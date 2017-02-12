56f00528e64eb244e19ad08a6eb7fc75c33e975d
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-11 15:37:16 -1000

    sanity check works

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,18 @@
 /*jshint esversion: 6 */
 const server = require('../server');
 const express = require('express');
-const gallery = require('../models/Gallery.js');
+const models = require('../models');
 
 const app = express();
 const router = express.Router();
+
+router.route('/')
+  .get((req, res) => {
+    models.Gallery.findAll()
+      .then((gallery) => {
+        // res.send('sanity check');
+        res.json(gallery);
+      });
+  });
 
 module.exports = router;
